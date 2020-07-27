3719b17534352bc874fb1b4aed7bbccd49f059ab
AlirezaKarimi <alireza.karimi.67@gmail.com>
2020-07-27 14:44:37 +0200

    fix

--- connection/mqtt-connection.js
+++ connection/mqtt-connection.js
@@ -1,36 +1,35 @@
+let mqtt = require("mqtt");
+require("dotenv/config");
 
-let mqtt = require('mqtt')
-require('dotenv/config');
+let mqttClient = function () {
+  let options = {
+    port: 1883,
+    clientId: "scanner/" + process.env.FLOOR + "/" + process.env.ROOM,
+  };
 
-let mqttClient= function () {
-    let options = {
-        port: 1885,
-        clientId:"scanner/"+process.env.FLOOR+"/"+process.env.ROOM
-    }
+  console.log("mqttClient");
+  let client = mqtt.connect("mqtt://127.0.0.1", options);
+  client.on("connect", function () {
+    console.log("mqttClient connect");
+  });
 
-    console.log('mqttClient');
-    let client = mqtt.connect('mqtt://127.0.0.1', options);
-    client.on('connect', function () {
-        console.log('mqttClient connect');
-    })
+  function publishMessage(topic, message) {
+    client.publish(topic, message);
+  }
 
-    function publishMessage(topic,message) {
-        client.publish(topic,message)
-    }
+  function subscribe(topic) {
+    client.subscribe(topic);
+  }
 
-    function subscribe(topic) {
-        client.subscribe(topic)
-    }
+  function unsubscribe(topic) {
+    client.unsubscribe(topic);
+  }
 
-    function unsubscribe(topic) {
-        client.unsubscribe(topic)
-    }
-
-    return{
-        publishMessage,
-        subscribe,
-        unsubscribe
-    }
-}
+  return {
+    publishMessage,
+    subscribe,
+    unsubscribe,
+  };
+};
 
 module.exports = mqttClient();
