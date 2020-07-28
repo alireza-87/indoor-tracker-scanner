c58793ad6b22a080c2b36473ae840e86da2eb845
AlirezaKarimi <alireza.karimi.67@gmail.com>
2020-07-28 17:24:01 +0200

    f

--- app.js
+++ app.js
@@ -1,57 +1,77 @@
-const storageHandler = require('./storage/storage-handler')
-const BeaconScanner = require('node-beacon-scanner');
-const mqttClient = require('./connection/mqtt-connection')
-require('dotenv/config');
-const cron = require("node-cron")
-const roomNumber=207
-let storage =new storageHandler()
-storage.init('sqlite')
-
+const storageHandler = require("./storage/storage-handler");
+const BeaconScanner = require("node-beacon-scanner");
+const mqttClient = require("./connection/mqtt-connection");
+require("dotenv/config");
+const cron = require("node-cron");
+const roomNumber = 207;
+let storage = new storageHandler();
+storage.init("sqlite");
 
 // Set an Event handler for becons
 const scanner = new BeaconScanner();
 scanner.onadvertisement = (obj) => {
-    try {
-        if (obj.beaconType === 'iBeacon') {
-            storage.emitterArrive(obj.beaconType, obj.iBeacon.uuid, obj.id, obj.address, Date.now(),function (row,uuid) {
-                if (row === "insert"){
-                    //mqttClient.publishMessage("/dibrisbuilding/occ","New -> "+uuid)
-                    mqttClient.subscribe("/dibrisbuilding/"+process.env.FLOOR+"/"+process.env.ROOM+'/'+uuid)
-                }
-            })
+  try {
+    if (obj.beaconType === "iBeacon") {
+      storage.emitterArrive(
+        obj.beaconType,
+        obj.iBeacon.uuid,
+        obj.id,
+        obj.address,
+        Date.now(),
+        function (row, uuid) {
+          if (row === "insert") {
+            //mqttClient.publishMessage("/dibrisbuilding/occ","New -> "+uuid)
+            mqttClient.subscribe(
+              "/dibrisbuilding/" +
+                process.env.FLOOR +
+                "/" +
+                process.env.ROOM +
+                "/" +
+                uuid
+            );
+          }
         }
-    }catch (e) {
-
+      );
     }
+  } catch (e) {}
 };
 
 // Start scanning
-scanner.startScan().then(() => {
-    console.log('Started to scan.')
-}).catch((error) => {
+scanner
+  .startScan()
+  .then(() => {
+    console.log("Started to scan.");
+  })
+  .catch((error) => {
     console.error(error);
-});
+  });
 
 //Check current beacon every 10 sec
-cron.schedule("*/1 * * * * *", function() {
-    storage.getAllEmitters(function (err,row) {
-        console.log("Running Cron Job");
-        const current_time=Date.now()-(1*1000);
-        if (!err)
-        row.forEach(function (item) {
-                if (item.timestamp<current_time) {
-                    console.log("uuid gone : %s", item.uuid);
-                    storage.deleteEmitter(item.uuid,function (err) {
-                        if (!err) {
-                            console.log("DELETE");
-                            //mqttClient.publishMessage("/dibrisbuilding/occ","Delete -> "+item.uuid)
-                            mqttClient.unsubscribe("/dibrisbuilding/"+process.env.FLOOR+"/"+process.env.ROOM+'/'+item.uuid)
-                        }else {
-                            console.log("DELETE ERROR");
-                        }
-                    })
-                }
-            })
-    })
-
+cron.schedule("*/1 * * * * *", function () {
+  storage.getAllEmitters(function (err, row) {
+    console.log("Running Cron Job");
+    const current_time = Date.now() - 1 * 2500;
+    if (!err)
+      row.forEach(function (item) {
+        if (item.timestamp < current_time) {
+          console.log("uuid gone : %s", item.uuid);
+          storage.deleteEmitter(item.uuid, function (err) {
+            if (!err) {
+              console.log("DELETE");
+              //mqttClient.publishMessage("/dibrisbuilding/occ","Delete -> "+item.uuid)
+              mqttClient.unsubscribe(
+                "/dibrisbuilding/" +
+                  process.env.FLOOR +
+                  "/" +
+                  process.env.ROOM +
+                  "/" +
+                  item.uuid
+              );
+            } else {
+              console.log("DELETE ERROR");
+            }
+          });
+        }
+      });
+  });
 });
